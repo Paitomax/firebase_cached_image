a44f434c55c27311b592151d4765a59c29f31b56
Guilherme Nascimento <paitomax@gmail.com>
2023-11-08 18:59:43 -0300

    Add firebase_image to export

--- lib/firebase_cached_image.dart
+++ lib/firebase_cached_image.dart
@@ -5,3 +5,4 @@ export 'package:firebase_cached_image/src/firebase_cache_manager/stub_firebase_c
     if (dart.library.html) "package:firebase_cached_image/src/firebase_cache_manager/web_firebase_cache_manager.dart"
     if (dart.library.io) "package:firebase_cached_image/src/firebase_cache_manager/mobile_firebase_cache_manager.dart";
 export 'package:firebase_cached_image/src/firebase_image_provider.dart';
+export 'package:firebase_cached_image/src/firebase_image.dart';
